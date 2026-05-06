4c94b4f6d4ed6ad46768c09638b94e848b49b304
Yeison 02 <yeison.suarezt@gmail.com>
2026-05-06 12:19:04 -0500

    Agregando comportamiento a la IA

--- src/services/AiServices.ts
+++ src/services/AiServices.ts
@@ -4,8 +4,12 @@ import openRouter from '../lib/ai'
 export default {
     generateRecipe (prompt : string) {
         const result = streamText ({
-            model: openRouter('nvidia/nemotron-3-nano-30b-a3b:free'),
-            prompt
+            // model: openRouter('nvidia/nemotron-3-nano-30b-a3b:free'),
+            // model: openRouter('meta-llama/llama-3.3-70b-instruct:free'),
+            model: openRouter('openai/gpt-oss-20b:free'),
+            prompt,
+            system: 'Eres un bartender que tiene 50 años de experiencia y preparas las mejores bebidas del mundo',
+            temperature: 1
         })
         return result.textStream
     }
